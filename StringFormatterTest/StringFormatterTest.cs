39c25779ef28f5430f1ab6d91fe8db5ead772182
Edwin Quai Hoi <edwinquaihoi@gmail.com>
2025-11-14 12:14:24 +1100

    Setup project structure with unit tests

---
+++ StringFormatterTest/StringFormatterTest.cs
@@ -0,0 +1,44 @@
+using Xunit;
+
+namespace PointsBet_Backend_Online_Code_Test 
+{
+
+    public class StringFormatterTest 
+    {
+        [Fact]
+        public void TestToCommaSeparatedList()
+        {
+            string result1 = StringFormatter.ToCommaSeparatedList(["A", "B", "C"], ",");
+            Assert.Equal("A,B,C", result1);
+        }
+
+        [Fact]
+        public void TestToCommaSeparatedListWithEmptySpace()
+        {
+            string result2 = StringFormatter.ToCommaSeparatedList(["A", "B", "C"], "");
+            Assert.Equal("ABC", result2);
+        }
+
+        [Fact]
+        public void TestToCommaSeparatedListWithEmptyList()
+        {
+            string result3 = StringFormatter.ToCommaSeparatedList([], "");
+            Assert.Equal("", result3);
+        }
+
+        [Fact]
+        public void TestToCommaSeparatedListWithMultiCharDelimiter()
+        {
+            string result4 = StringFormatter.ToCommaSeparatedList(["A", "B"], "!@#$");
+            Assert.Equal("A!@#$B", result4);
+        }
+
+        [Fact]
+        public void TestToCommaSeparatedListWithOneItem()
+        {
+            string result5 = StringFormatter.ToCommaSeparatedList(["A"], ",");
+            Assert.Equal("A", result5);
+        }
+
+    }
+}
