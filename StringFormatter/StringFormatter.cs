fc46401e104170cdff5e744a8a813843ef2918cc
Edwin Quai Hoi <edwinquaihoi@gmail.com>
2025-11-14 12:21:11 +1100

    Implement optimised solution

--- StringFormatter/StringFormatter.cs
+++ StringFormatter/StringFormatter.cs
@@ -16,17 +16,12 @@ namespace PointsBet_Backend_Online_Code_Test {
         //Code to improve
         public static string ToCommaSeparatedList(string[] items, string quote)
         {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
 
-            if (items.Length > 1)
-            {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
-            }
+            if (items?.Length == 0) return "";
 
-            return qry.ToString();
+            if (items?.Length > 1) return string.Join(quote, items);
+
+            return items == null ? "" : items.First();
         }
     }
 }
